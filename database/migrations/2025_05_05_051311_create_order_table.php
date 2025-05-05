5c0e212635d5d1dace198fdba8ca689f2f4b96d6
arhamullahkamaruddin <arhamullahav.com>
2025-05-05 13:41:44 +0800

    update migrate

--- database/migrations/2025_05_05_051311_create_order_table.php
+++ database/migrations/2025_05_05_051311_create_order_table.php
@@ -1,24 +1,22 @@
-<?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 class CreateOrderTable extends Migration
 {
-    public function up(): void
+    public function up()
     {
         Schema::create('orders', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->foreignId('course_id')->constrained('courses')->onDelete('cascade');
-            $table->decimal('amount', 10, 2);
-            $table->enum('status', ['pending', 'paid', 'completed', 'disputed', 'refunded']);
-            $table->timestamps();
+            $table->id(); // id bigint, otomatis auto increment
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); // foreign key ke tabel users
+            $table->foreignId('course_id')->constrained('courses')->onDelete('cascade'); // foreign key ke tabel courses
+            $table->decimal('amount', 10, 2); // jumlah yang dibayar, dengan presisi 10 dan 2 digit desimal
+            $table->enum('status', ['pending', 'paid', 'completed', 'disputed', 'refunded']); // status order
+            $table->timestamps(); // created_at, updated_at
         });
     }
 
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('orders');
     }
